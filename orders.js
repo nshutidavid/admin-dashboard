5d4f8c6a245ed8db9fbb8fba37ffe6d3b65c12ed
nshutidavid <nshutitoto@gmail.com>
2023-07-12 13:36:19 +0200

    first commit

---
+++ orders.js
@@ -0,0 +1,88 @@
+const Orders = [
+  {
+    productName: 'Foldable Mini Drone',
+    productNumber: '85631',
+    paymentStatus: 'Due',
+    shipping: 'pending',
+  },
+
+  {
+    productName: 'LARVENDER KF102 Drone',
+    productNumber: '363378',
+    paymentStatus: 'Refunded',
+    shipping: 'Declined',
+  },
+
+  {
+    productName: 'Ruko F11 pro drone',
+    productNumber: '49347',
+    paymentStatus: 'Due',
+    shipping: 'pending',
+  },
+
+  {
+    productName: 'Drone with camera Drone',
+    productNumber: '96996',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered',
+  },
+
+  {
+    productName: 'GPS 4k Drone',
+    productNumber: '22821',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered',
+  },
+
+  {
+    productName: 'DJ1 Air 25',
+    productNumber: '81475',
+    paymentStatus: 'Due',
+    shipping: 'pending',
+  },
+
+  {
+    productName: 'Lonzene Drone',
+    productNumber: '00482',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered',
+  },
+];
+
+/*  <tr>
+                <td>Foldable Mini Drone</td>
+                <td>85631</td>
+                <td>Due</td>
+                <td class="warning">Pending</td>
+                <td class="primary">Details</td>
+              </tr>
+
+              <tr>
+                <td>Foldable Mini Drone</td>
+                <td>85631</td>
+                <td>Due</td>
+                <td class="warning">Pending</td>
+                <td class="primary">Details</td>
+              </tr>
+
+              <tr>
+                <td>Foldable Mini Drone</td>
+                <td>85631</td>
+                <td>Due</td>
+                <td class="warning">Pending</td>
+                <td class="primary">Details</td>
+              </tr>
+              <tr>
+                <td>Foldable Mini Drone</td>
+                <td>85631</td>
+                <td>Due</td>
+                <td class="warning">Pending</td>
+                <td class="primary">Details</td>
+              </tr>
+              <tr>
+                <td>Foldable Mini Drone</td>
+                <td>85631</td>
+                <td>Due</td>
+                <td class="warning">Pending</td>
+                <td class="primary">Details</td>
+              </tr>*/
